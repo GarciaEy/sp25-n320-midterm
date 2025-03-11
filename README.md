c432f819e5610c4d9a0f4a002f627c139820b92e
Eyanla Garcia <eygarcia@iu.edu>
2025-03-11 17:03:10 -0400

    midterm

---
+++ README.md
@@ -0,0 +1,3 @@
+# SPRING 2025 MIDTERM | Eyanla Garcia | 3/11/25
+
+## live/render link:
---
+++ js/app.js
@@ -0,0 +1,37 @@
+import { jsPDF } from "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.0.0/jspdf.es.js";
+
+let myDoc = new jsPDF();
+
+let cart = [];
+
+function addCart(itemName, price) {
+  let cart = document.getElementById("#cart");
+  cart.push({ itemName, price });
+}
+
+class InvoicePDFGenerator {
+  constructor(invoice) {
+    this.invoice = invoice;
+    this.pdf = new jsPDF();
+  }
+
+  generate() {
+    this.pdf.text("Invoice", 10, 10);
+    this.pdf.text(`Date: ${this.invoice.date}`, 10, 20);
+    this.pdf.text(`Total: $${this.invoice.total}`, 10, 30);
+    this.pdf.save("mydoc.pdf");
+  }
+  downloadPdf() {
+    this.pdfDoc.save("mydoc.pdf");
+  }
+  viewPdf() {
+    this.pdfDoc.output("mydoc.pdf");
+  }
+}
+document.querySelector("#download").onclick = downloadPdf;
+
+document.querySelector("#view").onclick = viewPdf;
+
+document.querySelector("#cart").onclick = addCart;
+
+//man idk no matter what i try it wont work lolll
